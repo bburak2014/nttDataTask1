7952d41dfb5b7218cf6d691159a7e5aec0163165
bburak2014 <73018589+bburak2014@users.noreply.github.com>
2022-05-31 14:46:29 +0300

    nttTask1

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+ 
+export const getTodosAsync = createAsyncThunk(
+	'todos/getTodosAsync',
+	async () => {
+		const resp = await fetch('https://snetmyapp.herokuapp.com/case1');
+		if (resp.ok) {
+
+			const res = await resp.json();
+			const todos=res.offerList
+ 
+			return { todos };
+		}
+	}
+);
+
+
+export const todoSlice = createSlice({
+	name: 'todos',
+	initialState: [],
+	reducers: {
+		addTodo: (state, action) => {
+			const todo = {
+				id: nanoid(),
+				photoUrl: action.payload.photoUrl,
+				price: action.payload.price,
+				cardType: action.payload.cardType,
+				name: action.payload.name,
+				position: action.payload.position,
+				team: action.payload.team,
+				attributes: action.payload.attributes,
+			};
+
+			state.push(todo);
+
+		},
+		toggleComplete: (state, action) => {
+			const index = state.findIndex((todo) => todo.id === action.payload.id);
+			state[index].completed = action.payload.completed;
+		},
+		deleteTodo: (state, action) => {
+			return state.filter((todo) => todo.id !== action.payload.id);
+		},
+		CategoryTodo: (state, action) => {
+			return state.filter((todo) => todo.cardType === action.payload.cardType);
+		},
+	},
+	extraReducers: {
+		[getTodosAsync.fulfilled]: (state, action) => {
+			return action.payload.todos;
+		}
+		
+	},
+});
+ 
+
+
+
+export const { addTodo, toggleComplete, deleteTodo,CategoryTodo } = todoSlice.actions;
+ 
+export default todoSlice.reducer;
